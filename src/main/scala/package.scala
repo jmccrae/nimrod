a6feae15cd49acfee316e8132f000baf24783e4b
John McCrae <john@mccr.ae>
2013-08-08 16:07:37 +0200

    Add some pimps for files

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -1,7 +1,13 @@
+import java.io.File
+
 package object nimrod {
   implicit def stringPimps(s : String) = new {
     def %(args : AnyRef*) = String.format(s,args:_*)
     def ls = new java.io.File(s).list()
+  }
+  implicit def filePimps(f : File) = new {
+    def ls = f.list()
+    def path = f.getCanonicalPath()
   }
   def set(key : String, value : String) {
     System.setProperty(key,value)
