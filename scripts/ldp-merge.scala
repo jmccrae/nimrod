c235a4e600129fd728af90fb062cd597fb53a21e
John McCrae <john@mccr.ae>
2013-08-08 17:39:40 +0100

    Add LDP translate method

--- scripts/ldp-merge.scala
+++ scripts/ldp-merge.scala
@@ -1,22 +1,29 @@
-if(args.length != 2) {
-  System.err.println("Usage:\n\tscala scripts/ldp-merge.scala labels translations < ldp")
-  System.exit(-1)
-}
+val labelFile = opts.roFile("labels","The file containing the labels, which were translated")
+val transFile = opts.roFile("translations","The translations of the labels")
+val ldpFile = opts.woFile("ldpFile","The source ldpFile")
+val out = opts.outFileOrStdout()
+opts.verify
 
-val srcIn = io.Source.fromFile(args(0)).getLines
-val trgIn = io.Source.fromFile(args(1)).getLines.map(_.replaceAll("@...?$",""))
+namedTask("Update LDP") {
+  val srcIn = io.Source.fromFile(labelFile).getLines
+  val trgIn = io.Source.fromFile(transFile).getLines.map(_.replaceAll("@...?$",""))
 
-val translations = (srcIn zip trgIn).toMap
+  val translations = (srcIn zip trgIn).toMap
 
-val rdfLiteral = java.util.regex.Pattern.compile("\"([^\"]+)\"" + (if(args.length == 1) { "@" + args(0) } else { "" }))
+  val rdfLiteral = java.util.regex.Pattern.compile("\"([^\"]+)\"")
 
-for(line <- io.Source.stdin.getLines) {
-  val toTranslate = collection.mutable.ListBuffer[String]()
-  val m = rdfLiteral.matcher(line)
-  while(m.find()) {
-    toTranslate += m.group(1)
+  for(line <- opts.openInput(ldpFile).getLines) {
+    val toTranslate = collection.mutable.ListBuffer[String]()
+    val m = rdfLiteral.matcher(line)
+    while(m.find()) {
+      toTranslate += m.group(1)
+    }
+    var newLine = line
+    for(foreign <- toTranslate) {
+      newLine = newLine.replace("\""+foreign+"\"",translations(foreign))
+    }
+    out.println(newLine)
   }
-  for(foreign <- toTranslate.toList) {
-    line.replace("\""+foreign+"\"","\"" + translations(foreign) + "\"")
-  }
+  out.flush
+  out.close
 }
