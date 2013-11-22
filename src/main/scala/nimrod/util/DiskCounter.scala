a12e89e4f5ec8b4c051ca8cf860dcdad48485401
John McCrae <john@mccr.ae>
2013-11-21 20:57:17 -0500

    Disk based iteration based on merge sort

--- src/main/scala/nimrod/util/DiskCounter.scala
+++ src/main/scala/nimrod/util/DiskCounter.scala
@@ -1,38 +1,140 @@
 package nimrod.util
 
-import org.mapdb.DB
-import scala.collection.JavaConversions._
+import java.io._
 import java.lang.Integer
+import java.util.TreeMap
+import scala.actors._
+import scala.collection.JavaConversions._
 
 /**
  * An implementation of a counter that is disk-based and increments (and is thread-safe)
  */
-class DiskCounter[A](sorted : Boolean = false, db : DB = DiskCounter.createDB()) {
-  private val name = math.abs(math.random * 1000000000).toString
-  private val theMap = if(sorted) {
-    db.getTreeMap[A,Integer](name)
-  } else {
-    db.getHashMap[A,Integer](name)
-  }
-
-  def inc(key : A) = {
-    // First check if there is a value by trying to put 1
-    var v = theMap.putIfAbsent(key,1)
-    if(v != null) {
-      // Guess there was, let's try to increment this value
-      while(!theMap.replace(key,v,v+1)) {
-        // Nope, changed again, reread the map
-        v = theMap.get(key)
+class DiskCounter[A](writeInterval : Int = 1000000)(implicit ordering : Ordering[A]) {
+  private var theMap = new TreeMap[A,Integer](ordering)
+  private var files : List[File] = Nil
+  private case class Inc(key : A)
+  private object Values
+  private object Close
+  private case class Sync(map : TreeMap[A,Integer])
+  private val mapLock = new Object
+  private val incActor = new Actor {
+    def act {
+      var received = 0
+      var done = false
+      while(true) {
+        receive {
+          case Inc(key) => {
+            if(received > writeInterval) {
+            //  mapLock.synchronized {
+                syncActor !? Sync(theMap)
+                theMap = new TreeMap[A,Integer](ordering)
+             // }
+              received = 0
+            } 
+           // mapLock.synchronized {
+            theMap.get(key) match {
+              case null => {
+                theMap.put(key,1)
+                received += 1
+              }
+              case s => theMap.put(key,s+1)
+            }
+            //}
+          }
+          case Values => {
+           // mapLock.synchronized {
+            syncActor !? Sync(theMap)
+            theMap = new TreeMap[A,Integer](ordering)
+            //}
+            reply(new ValueIterator(files))
+          }
+          case Close => {
+            done = true
+            reply(true)
+          }
+        }
       }
     }
   }
-  def get(key : A) : Int = theMap.get(key).intValue
-  def values : Iterator[(A,Int)] = theMap.iterator.map {
-    case (x,y) => (x,y.intValue)
+  incActor.start
+  private val syncActor = new Actor {
+    def act {
+      var done = false
+      loop {
+        react {
+          case Sync(map) => try {
+            writeMap(map)
+            reply(Unit)
+          } catch {
+            case x : Exception => x.printStackTrace
+          }
+          case Close => {
+            done = true
+            reply(true)
+          }
+        }
+      }
+    }
   }
-}
+  syncActor.start
+  
+  private def writeMap(map : TreeMap[A,Integer]) {
+    val outFile = File.createTempFile("diskcounter",".bin")
+    outFile.deleteOnExit
+    val out = new ObjectOutputStream(new FileOutputStream(outFile))
+    out.writeInt(map.size)
+    val entryIterator = map.entrySet().iterator()
+    while(entryIterator.hasNext) {
+      val entry = entryIterator.next()
+      val key = entry.getKey()
+      val value = entry.getValue()
+      out.writeObject(key)
+      out.writeInt(value)
+    }
+    out.flush
+    out.close
+    files ::= outFile
+  }
 
-object DiskCounter {
-  def createDB(cacheSize : Int = 1048576) = org.mapdb.DBMaker.newTempFileDB().cacheSize(cacheSize).cacheLRUEnable().make()
-  def closeDB(db : DB) = db.close
+  private class SerializedMapIterator(file : File) extends Iterator[(A,Int)] {
+    val in = new ObjectInputStream(new FileInputStream(file))
+    val _size = in.readInt()
+    var read = 0
+
+    def next = {
+      read += 1
+      (in.readObject().asInstanceOf[A],in.readInt())
+    }
+    def hasNext = read < _size
+  }
+
+  private class ValueIterator(files : List[File]) extends Iterator[(A,Int)] {
+    val readers = files map (file => new PeekableIterator[(A,Int)](new SerializedMapIterator(file)))
+
+    def next = {
+      val minVal = readers.flatMap(_.peek.map(_._1)).min(ordering)
+      val active = readers filter { 
+        _.peek match {
+          case Some((v,_)) => minVal == v
+          case None => false
+        }
+      }
+      require(!active.isEmpty)
+      val sum = active.map(_.peek.get._2).sum
+      val key = active.head.peek.get._1
+      active.foreach(_.next)
+      (key,sum)
+    }
+    def hasNext = readers.exists(_.hasNext)
+  }
+  
+  /** Increment a key (non-blocking) */
+  def inc(key : A) = incActor ! Inc(key)
+  /** Get all values written so far (this may block) */
+  def values : Iterator[(A,Int)] = (incActor !? Values).asInstanceOf[Iterator[(A,Int)]]
+  /** Call this to shutdown the actor */
+  def close = {
+    incActor !? Close
+    syncActor !? Close
+  }
 }
---
+++ src/main/scala/nimrod/util/PeekableIterator.scala
@@ -0,0 +1,28 @@
+package nimrod.util
+
+class PeekableIterator[A](base : Iterator[A]) extends Iterator[A] {
+  private var last : Option[A] = None
+
+  override def next = last match {
+    case Some(a) => {
+      last = None
+      a
+    }
+    case None => base.next
+  }
+
+  override def hasNext = last != None || base.hasNext
+
+  def peek : Option[A] = {
+    last match {
+      case Some(_) => 
+      case None => {
+        if(base.hasNext) {
+          last = Some(base.next)
+        }
+      }
+    }
+    last
+  }
+}
+
