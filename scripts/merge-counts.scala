6f901ac5a0f608e264f73ba47179df66a001769e
John McCrae <john@mccr.ae>
2014-01-02 17:33:26 +0100

    Fix bug with duplicates in merge

--- scripts/merge-counts.scala
+++ scripts/merge-counts.scala
@@ -9,20 +9,20 @@ namedTask("Merge counts") {
   while(readers.exists(_.hasNext)) {
     val lines = readers.flatMap(_.peek)
     val keyCounts = lines.map( line => {
-      (line.substring(0,line.lastIndexOf("|||") + 4), line.substring(line.lastIndexOf("|||")+4).toInt)
+      (line.substring(0,line.lastIndexOf("|||")), line.substring(line.lastIndexOf("|||") + 4).toInt)
     })
     val minKey = keyCounts.map(_._1).min
     val total = keyCounts.filter(_._1==minKey).map(_._2).sum
     val active = readers.filter { 
       reader => {
         reader.peek match {
-          case Some(line) => line.startsWith(minKey)
+          case Some(line) => line.substring(0,line.lastIndexOf("|||")) == minKey
           case None => false
         }
       }
     }
     active.foreach(_.next)
-    out.println(minKey + total)
+    out.println(minKey + "||| " + total)
   }
   out.flush
   out.close
