9a1cc9ebc766d5f0b2aeb9b38a50af2c90522b45
John McCrae <john@mccr.ae>
2014-01-07 13:26:52 +0100

    Remove scripts and utils

--- scripts/build-moses.scala
+++
@@ -1,8 +0,0 @@
-import nimrod._
-import nimrod.tasks._
-
-mkdir("moses") exec
-
-wget("http://giza-pp.googlecode.com/files/giza-pp-v1.0.7.tar.gz") > "moses/giza-pp-v1.0.7.tar.gz" exec
-
-tarxz("moses/giza-pp-v1.0.7.tar.gz") exec
